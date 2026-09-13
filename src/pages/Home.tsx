1053cfda33763c23720fb4e00a42a6a8ba8f9e51
caiomarqx <caio.marques111107@gmail.com>
2026-09-13 14:17:31 -0300

    feat: implementa useEffect na Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
 import HeroSection from '../components/Home/HeroSection'
 
 function Home() {
+  useEffect(() => {
+    const tituloAnterior = document.title
+
+    document.title = 'SoulPass | Início'
+
+    return () => {
+      document.title = tituloAnterior
+    }
+  }, [])
+
   return (
     <HeroSection />
   )
